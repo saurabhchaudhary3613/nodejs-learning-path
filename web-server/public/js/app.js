aaeab24cba39a57cf1efb4d5b1724fbc2bed552f
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-07-23 15:58:56 +0530

    front end and service integration

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
-console.log('client side js');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+    
+    res.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data[0].location)
+            console.log(data[0].forecast)
+        }
+    })
+})
+})
