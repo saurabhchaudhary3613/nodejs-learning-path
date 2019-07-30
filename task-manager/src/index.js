76104dc2c15b9c485837756ac4aca7f63986493a
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-07-30 23:39:21 +0530

    Reading endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,6 +21,30 @@ app.post('/users', (req, res) => {
     })
 })
 
+
+//Reading/fetching all users
+app.get('/users', (req, res) => {
+    User.find({}).then((users) =>{
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Read/fetch specific user
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user) {
+           return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Create a new tssk using post
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -31,6 +55,29 @@ app.post('/tasks', (req, res) => {
     }) 
 })
 
+// Read/fetch all tasks from database
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+// Read/fetch specific task by id from database
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is up on ' + port)
