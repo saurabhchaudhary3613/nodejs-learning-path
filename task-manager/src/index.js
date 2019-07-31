48edb39400164e86974bf280505c8fee3abc1355
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-07-31 15:45:23 +0530

    code refactor with async await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,73 +10,100 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
 
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((error) => {
-        res.status(400)
-        res.send(error)
-    })
+    } catch(error) {
+        res.status(400).send(error)
+    }
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch((error) => {
+    //     res.status(400)
+    //     res.send(error)
+    // })
 })
 
 
 //Reading/fetching all users
-app.get('/users', (req, res) => {
-    User.find({}).then((users) =>{
+app.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch(error) {
         res.status(500).send()
-    })
+    }
+
+    // User.find({}).then((users) =>{
+    //     res.send(users)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 // Read/fetch specific user
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+
+    try {
+       const user =  await User.findById(_id)
         if(!user) {
-           return res.status(404).send()
+            return res.status(404).send()
         }
         res.send(user)
-    }).catch((e) => {
+    } catch(e) {
         res.status(500).send()
-    })
+    }
+    // User.findById(_id).then((user) => {
+    //     if(!user) {
+    //        return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-// Create a new tssk using post
-app.post('/tasks', (req, res) => {
+// Create a new task using post
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error);
-    }) 
+    } catch(error) {
+        res.status(400).send(error)
+    } 
 })
 
 // Read/fetch all tasks from database
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+    } catch(e) {
+        res.status(500).send(e)
+    }
 })
 
 // Read/fetch specific task by id from database
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if(!task) {
             return res.status(404).send()
         }
-        res.send(task)
-
-    }).catch((e) => {
-        res.status(500).send()
-    })
+        res.status(404).send(task)
+    } catch(e) {
+        res.status(500).send(e)
+    }
 })
 
 app.listen(port, () => {
