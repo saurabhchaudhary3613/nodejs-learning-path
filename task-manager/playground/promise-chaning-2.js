e1673bdedf118e2c990f3fb033a5d02f81aed91f
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-07-31 14:31:05 +0530

    async await learning

--- task-manager/playground/promise-chaning-2.js
+++ task-manager/playground/promise-chaning-2.js
@@ -2,11 +2,25 @@ require('../src/db/mongoose')
 
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5d403d5fde5f5f0aac02c26b').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5d403d5fde5f5f0aac02c26b').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+// using async await
+const deleteAndCounttask = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed: false})
+
+    return count
+}
+
+deleteAndCounttask('5d4079ea98fa180349eb1b5f').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
--- task-manager/playground/promise-chaning.js
+++ task-manager/playground/promise-chaning.js
@@ -2,11 +2,26 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5d403a252bdbc07ed8ea28e5', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1})
-}).then((result) => {
-    console.log(result)
+// Using Promise
+// User.findByIdAndUpdate('5d403a252bdbc07ed8ea28e5', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+//Using async await
+const updateAgeAndCount = async (id, age) => {
+
+    const user = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age})
+    return count
+}
+
+updateAgeAndCount('5d403a252bdbc07ed8ea28e5', 20).then((count) => {
+    console.log(count)
 }).catch((e) => {
-    console.log(e)
-})
+    console.log(e);
+}) 
