556855f66fa43b61fb773c91c28b379030b32e6a
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-07-31 18:24:48 +0530

    Working with Delete verb

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -69,7 +69,6 @@ app.get('/users/:id', async (req, res) => {
 })
 
 //update exiting resource/user using patch
-
 app.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowupdates = ['email', 'name', 'age', 'password']
@@ -93,6 +92,22 @@ app.patch('/users/:id', async (req, res) => {
     } catch(e) {
         res.status(400).send(e)
     }
+})
+
+// Delete a resource/user by Id
+
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user) {
+            return res.status(404).send()
+        }
+         res.send(user)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+    
 })
 
 // Create a new task using post
@@ -156,6 +171,21 @@ app.patch('/tasks/:id', async (req, res) => {
     } catch(e) {
         res.status(400).send(e)
     }
+})
+
+//Delete task by Id
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task) {
+            return res.status(404).send()
+        }
+         res.send(task)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+    
 })
 
 app.listen(port, () => {
