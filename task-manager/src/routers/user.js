ccb2e5aeba85fbf303da93bf75e83b0cf75d1200
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-08-01 15:22:09 +0530

    saved token to database

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -8,7 +8,8 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch(error) {
         res.status(400).send(error)
     }
@@ -24,7 +25,8 @@ router.post('/users/login', async (req, res) => {
 
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
     } catch (e) {
         res.status(404).send()
     }
