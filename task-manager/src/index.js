21af74e60b6c585bf3b94cb6172d942ec29f2583
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-08-01 18:38:27 +0530

    Accepting Authentication token

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,18 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET Request are disabled !')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -33,20 +34,18 @@ router.post('/users/login', async (req, res) => {
 })
 
 //Reading/fetching all users
-router.get('/users', async (req, res) => {
+// router.get('/users', auth, async (req, res) => {
 
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch(error) {
-        res.status(500).send()
-    }
+//     try {
+//         const users = await User.find({})
+//         res.send(users)
+//     } catch(error) {
+//         res.status(500).send()
+//     }
+// })
 
-    // User.find({}).then((users) =>{
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 // Read/fetch specific user
