ded3850f488ebaffd3919e67faf7b8864b60b5f8
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-08-04 01:58:57 +0530

    Logout router

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -33,6 +33,32 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+// Logout router
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
 //Reading/fetching all users
 // router.get('/users', auth, async (req, res) => {
 
