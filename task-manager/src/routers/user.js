271a6f028f0480f8a73ebf364759b6aec0b222e0
Saurabh <saurabh.chaudhary@publicissapient.com>
2019-08-05 19:09:07 +0530

    serving images to client

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -159,4 +159,20 @@ router.delete('/user/me/avatar', auth, async (req, res) => {
     res.send()
 })
 
+router.get('/users/:id/avatar', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+
+    } catch(e) {
+        res.status(404).send()
+    }
+})
+
 module.exports = router
